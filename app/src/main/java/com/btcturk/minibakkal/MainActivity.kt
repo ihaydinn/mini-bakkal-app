81d635bc88601d958fe7e4cf6806483c0a7d0be7
ihaydinn <ihaydin7@gmail.com>
2021-12-27 23:19:46 +0300

    Add nav graph fragment

--- app/src/main/java/com/btcturk/minibakkal/MainActivity.kt
+++ app/src/main/java/com/btcturk/minibakkal/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.btcturk.minibakkal
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.fragment)
+
     }
 }
